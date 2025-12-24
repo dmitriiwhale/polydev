50a2f44ffdf409dd85f5720884e9743456487ded
self <186314011+seelffff@users.noreply.github.com>
2025-12-24 23:25:49 +0300

    last commit

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -3,8 +3,28 @@ import inquirer from 'inquirer'
 import {Arms} from '../components/arms'; 
 
 
-function printContacts() { 
+
+const printBanner = () => { 
+    console.clear()
+    console.log(chalk.magenta.underline(`
+                                                                                                                        
+    _/_/_/      _/_/    _/    _/      _/  _/      _/    _/_/      _/_/_/  _/    _/  _/_/_/  _/      _/  _/_/_/_/   
+   _/    _/  _/    _/  _/      _/  _/    _/_/  _/_/  _/    _/  _/        _/    _/    _/    _/_/    _/  _/          
+  _/_/_/    _/    _/  _/        _/      _/  _/  _/  _/_/_/_/  _/        _/_/_/_/    _/    _/  _/  _/  _/_/_/       
+ _/        _/    _/  _/        _/      _/      _/  _/    _/  _/        _/    _/    _/    _/    _/_/  _/            
+_/          _/_/    _/_/_/_/  _/      _/      _/  _/    _/    _/_/_/  _/    _/  _/_/_/  _/      _/  _/_/_/_/       
+                                                                                                                                                                                                                                   
+        
+        `))
+    console.log('Algo and Copy trade machine');
+}
+
+const printContacts = () => { 
     console.log(chalk.magenta.underline('Twitter: https://x.com/seelffff'))
     console.log(chalk.magenta.underline('GitHub: https://github.com/seelffff'))
     console.log(chalk.magenta.underline('Sub pls ^_^'));
+}
+
+async function startCLI (): Promise<void> { 
+
 }
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,10 +5,11 @@ export const CLOB_CONFIG = {
     host: process.env.CLOB_HOST || 'https://clob.polymarket.com',
     chainId: parseInt(process.env.CLOB_CHAIN_ID || '137'),
     signatureType: parseInt(process.env.CLOB_SIGNATURE_TYPE || '0'),
-    defaultTickSize: process.env.DEFAULT_TICK_SIZE || '0.000001',
+    defaultTickSize: process.env.DEFAULT_TICK_SIZE || '0.001',
     defaultNegRisk: process.env.DEFAULT_NEG_RISK === 'true' || false,
     defaultOrderType: process.env.DEFAULT_ORDER_TYPE || 'GTC'
-    
+
+
   };
   
 
